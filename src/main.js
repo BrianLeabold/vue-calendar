c7c96af46b99843075caf3c0d1e5a99d993c2f84
BrianLeabold <livingsingl@hotmail.com>
2019-10-19 19:43:30 -0700

    Calendar Version 1

--- src/main.js
+++ src/main.js
@@ -1,10 +1,28 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 import vuetify from './plugins/vuetify';
+import VueTextareaAutosize from 'vue-textarea-autosize';
+import firebase from 'firebase/app';
+import 'firebase/firebase-firestore';
 
-Vue.config.productionTip = false
+Vue.use(VueTextareaAutosize);
+
+Vue.config.productionTip = false;
+
+firebase.initializeApp({
+  apiKey: "AIzaSyCfzgunQa0rxZLNdBRIkb1NukuN7CmJ7vw",
+  authDomain: "consultbrian-calendar.firebaseapp.com",
+  databaseURL: "https://consultbrian-calendar.firebaseio.com",
+  projectId: "consultbrian-calendar",
+  storageBucket: "consultbrian-calendar.appspot.com",
+  messagingSenderId: "471100370577",
+  appId: "1:471100370577:web:3a0c1f6ea24dd38d6bd930",
+  measurementId: "G-GQX4X7FSG7"
+});
+
+export const db = firebase.firestore();
 
 new Vue({
   vuetify,
   render: h => h(App)
-}).$mount('#app')
+}).$mount('#app');
